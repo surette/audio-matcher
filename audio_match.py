b133b1700c28e8e05cc957dba4f298263b50a7c2
Scott <scott@scottsurette.com>
2013-10-10 16:14:11 -0400

    Update and rename p4500 to audio_match.py

---
+++ audio_match.py
@@ -0,0 +1,9 @@
+# open file
+# check that file is actually a wav file
+# apply FFT using numpy.fft
+# compare two resulting arrays
+
+import sys
+
+file1 = open(sys.argv[0])
+file2 = open(sys.argv[1])
