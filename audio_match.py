f2d88083057ac653ea2387fb5043095eeb96deca
mhmdfy <mhmdfy@lethalenforcers.ccs.neu.edu>
2013-10-10 16:42:45 -0400

    verify file path function

--- audio_match.py
+++ audio_match.py
@@ -4,16 +4,26 @@
 # compare two resulting arrays
 
 import sys
+import os
 
 def verifyArgs():
 	if len(sys.argv) != 3:
 		print("Wrong number of arguments")
-		quit()
+		exit()
+
+def verifyPathToFile(path):
+	if not os.path.isfile(path):
+		print("Path "+path+" is incorrect/is not a file")
+		exit()
 
 def match():
 	verifyArgs()
+	verifyPathToFile(sys.argv[1])
+	verifyPathToFile(sys.argv[2])
 	file1 = open(sys.argv[1])
 	file2 = open(sys.argv[2])
 
+	file1.close()
+	file2.close()
 
 match()
