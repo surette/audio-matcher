d86849bc570b52e5bed26546659a6a3e7673364a
Dan McNamara <dmcnamara1@gmail.com>
2013-10-14 17:53:09 -0400

    our own allclose that works sometimes

--- fft_test2.py
+++ fft_test2.py
@@ -45,6 +45,7 @@ def match():
 
    # get a list of tuple of significant magnitudes for second 
    file1_mag = significantMags(file1_freq)
+   print "file2:"
    file2_mag = significantMags(file2_freq)
 
    # compare the two arrays of frequencies
@@ -77,12 +78,10 @@ def fftconvert(file):
    # the result array of fft for each second
    ffta = []
    chunksize = framerate * nchannels
-   print
    for i in range(0, nframes/chunksize):
       waveData = file.readframes(chunksize)
       data = struct.unpack_from("%dh" % chunksize, waveData)
       # print data
-      mags = np.fft.fft(data)
       mags = abs(np.fft.fft(data))**2
       freqs = np.fft.fftfreq(chunksize)
       ffta.append(zip(mags, freqs))
@@ -94,31 +93,55 @@ def significantMags(fft):
    result = []
    for sec in fft:
       tup = []
-      tup.append(highestMag(sec, 40, 80))
-      tup.append(highestMag(sec, 80, 120))
-      tup.append(highestMag(sec, 120, 180))
-      tup.append(highestMag(sec, 180, 300))
+      tup.append(highestMag(sec, -0.5, -0.25))
+      tup.append(highestMag(sec, -0.25, 0))
+      tup.append(highestMag(sec, 0, 0.25))
+      tup.append(highestMag(sec, 0.25, 0.5))
       result.append(tup)
 
    return result
 
+#fft1 and fft2 are arrays of tuples of highest mags defined in significiantMags
 def compare(fft1, fft2):
    return isSubset(fft1, fft2) or isSubset(fft2, fft1)
 
+#assumes list1 is the child/subset
 def isSubset(list1, list2):
-   for val in list1:
-      if val not in list2:
+   for val1 in list1:
+      # print val1
+      found = False
+      for val2 in list2:
+         # print val2
+         print "here"
+         if eachisclose(val1, val2):
+            found = True
+            print "is close"
+            break
+
+      if not found:
+         return False
+      # if val not in list2:
+      #    return False
+   return True
+def eachisclose(val1,val2):
+   off = 1e10
+
+   for v1,v2 in zip(val1,val2):
+      if -off <= v1-v2 <=  off:
+         pass
+      else:
          return False
    return True
+
 
 def highestMag(sec, low, high):	
    score = 0
    for mag, freq in sec:
       # print mag, freq
-      if low <= freq <= high:
+      if low <= freq < high:
          if mag > score:
             score = mag
-   print score
+   # print score
    return score
    
 if __name__ == '__main__':
