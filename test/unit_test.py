ad7159586df1181e84d563fb32a70a6d6c55e079
Dan McNamara <dmcnamara1@gmail.com>
2013-10-15 17:03:30 -0400

    test fixes

--- test/unit_test.py
+++ test/unit_test.py
@@ -5,9 +5,9 @@ import os
 import time
 import unittest
 
-dir = os.path.dirname(__file__)
-filename = os.path.join(dir, '../')
-print filename
+# dir = os.path.dirname(__file__)
+# filename = os.path.join(dir, '../')
+# print filename
 sys.path.append('../')
 import audio_match as p4500
 
@@ -20,7 +20,7 @@ class TestBasics(unittest.TestCase):
 		self.assertTrue(p4500.match('../A4/trouble.wav', '../A4/x1.wav'))
 
 	def test_matches_altered_subsets(self):
-		self.assertTrue(p4500.match('../A4/trouble1.wav', '../A4/x2.wav'))
+		self.assertTrue(p4500.match('../A4/trouble.wav', '../A4/x2.wav'))
 
 	def test_is_not_a_match(self):
 		self.assertFalse(p4500.match('../A4/x1.wav', '../A4/x10.wav'))
